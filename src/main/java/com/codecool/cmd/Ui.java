14859122484f130d89e33ee8e3ac9777480c43b7
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-03-26 10:36:06 +0100

    Ui class : first submenu

--- src/main/java/com/codecool/cmd/Ui.java
+++ src/main/java/com/codecool/cmd/Ui.java
@@ -1,5 +1,6 @@
 package com.codecool.cmd;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ui {
@@ -27,14 +28,19 @@ public class Ui {
             }
             switch (menuOption) {
                 case 1:
+                    int subChoice;
+                    do {
+                        System.out.println("Select an option!");
+                        System.out.println("1. Play");
+                        System.out.println("2. Stop");
+                        System.out.println("3. Add CD");
+                        System.out.println("4. Change track");
+                        System.out.println("5. Show information");
+                        System.out.println("0. Back");
+                        subChoice = getAnInteger();
+                    }while(subChoice != 0);
                     break;
                 case 2:
-
-                    break;
-                case 3:
-
-                    break;
-                case 4:
 
                     break;
             }
@@ -56,5 +62,16 @@ public class Ui {
         String input = scanner.nextLine();
         System.out.println();
         return input;
+    }
+
+    public int getAnInteger() {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.print("Give a valid number");
+            }
+        }
     }
 }
