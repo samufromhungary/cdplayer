3b71bd5fcc8ea3cb0441450f0d02451408bf14ee
samufromhungary <blzkproblem@gmail.com>
2019-03-27 11:27:46 +0100

    init

--- src/main/java/com/codecool/cmd/Writer.java
+++ src/main/java/com/codecool/cmd/Writer.java
@@ -1,6 +1,25 @@
 package com.codecool.cmd;
 
+import java.util.concurrent.TimeUnit;
+
 public abstract class Writer {
 
-    public void write(){};
+    public Writer(){
+
+    }
+
+    public void writemsg(String msg){
+        System.out.println(msg);
+    }
+
+    public void writeinfo(String title, String length){
+        System.out.println("Title: " + title + " | Length: " + TimeUnit.MINUTES.toSeconds(Long.parseLong(length)));
+    }
+
+    public void whatsPlaying(String title){
+        System.out.println("Playing " + title + "...");
+    }
+
+    public void randomAds(){
+    }
 }
