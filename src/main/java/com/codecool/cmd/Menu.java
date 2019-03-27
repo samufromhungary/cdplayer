e0d169a446cc6ea4de9f910914880f5982222086
samufromhungary <blzkproblem@gmail.com>
2019-03-27 11:20:22 +0100

    init

--- src/main/java/com/codecool/cmd/Menu.java
+++ src/main/java/com/codecool/cmd/Menu.java
@@ -4,14 +4,34 @@ import java.util.List;
 
 public class Menu {
 
+
+    private String title;
+    private String[] options;
     private List<MenuItem> items;
     private List<Writer> writers;
+
+    public Menu(String title, String[] options){
+        this.title = title;
+        this.options = options;
+    }
 
     public void addItem(MenuItem item){
         items.add(item);
     }
 
-    public void show(){}
+    public void show(){
+        System.out.print(title + "\n\n");
+        int i = 1;
+        for (String option:options) {
+            if (option.equals(options[options.length-1])) {
+                System.out.println("(" + i + ") " + option + "\n\n");
+                i++;
+            } else {
+                System.out.print("(" + i + ") " + option + "\n\n");
+                i++;
+            }
+        }
+    }
 
     public void addWriter(Writer writer){
         writers.add(writer);
