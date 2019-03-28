fc37f96d031d3cf88dfcfea447d55d90583a527b
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-03-28 10:07:40 +0100

    Merge branch 'master' of /home/ktarasz/Dokumentumok/java/cdplayer with conflicts.

--- src/main/java/com/codecool/cmd/Player.java
+++ src/main/java/com/codecool/cmd/Player.java
@@ -7,10 +7,6 @@ public class Player {
     private List<MenuItem> items;
     private List<Writer> writers;
     ConsoleWriter consoleWriter = new ConsoleWriter();
-    private Song currentSong;
-    private List<Song> songs;
-    Audio audioCd = new Audio();
-    ChangeTrackActivity activity = new ChangeTrackActivity();
 
 
     public void setMode(PlayerMode mode){
@@ -33,27 +29,11 @@ public class Player {
 
     public void show(){}
 
-    public Song prev(int i){
-        consoleWriter.writemsg("Changing to previous track");
-        if(i - 1 < 0){
-            currentSong = songs.get(songs.size() - 1);
-        }else{
-            currentSong = songs.get(i - 1);
-        }
-        return currentSong;
-    }
-
-    public Song next(int i){
-        consoleWriter.writemsg("Changing to next track");
-        if(i + 1 > songs.size() - 1){
-            currentSong = songs.get(0);
-        }else{
-            currentSong = songs.get(i + 1);
-        }
-        return currentSong;
-
+    public void prev(){
 
     }
+
+    public void next(){}
 
 
 }
